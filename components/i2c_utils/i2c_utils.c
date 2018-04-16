2301d63484bfea9a61c699a899bc3bcc7053af0c
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2018-04-16 09:48:42 -0300

    updated functions

--- components/i2c_utils/i2c_utils.c
+++ components/i2c_utils/i2c_utils.c
@@ -1,285 +1,144 @@
-/*
- * @files esp32_i2c_rw.c and esp32_i2c_rw.h
- * @author Gabriel Boni Vicari (133192@upf.br)
+/**
+ * @file i2c_utils.c
+ * 
+ * @author
+ * Gabriel Boni Vicari (133192@upf.br)
+ * Angelo Elias Dalzotto (150633@upf.br)
  * GEPID - Grupo de Pesquisa em Cultura Digital (http://gepid.upf.br/)
  * Universidade de Passo Fundo (http://www.upf.br/)
+ * 
+ * @copyright
+ * Copyright (c) 2012 Jeff Rowberg.
+ * 
  * @brief I2C Read/Write functions for ESP32 ESP-IDF.
  *
- * Based on I2Cdevlib's ESP32_ESP-IDF by Dariusz Kempa.
+ * @details Based on I2Cdevlib's ESP32_ESP-IDF by Dariusz Kempa.
  * I2Cdevlib's ESP32_ESP-IDF is based on I2Cdevlib's EFM32 by Nicolas Baldeck.
  * I2Cdevlib's EFM32 is based on I2Cdevlib's Arduino by Jeff Rowberg.
- */
-
-/*
---------------------------------------------------------------------------------
-I2Cdev device library code is placed under the MIT license.
-
-Copyright (c) 2012 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
---------------------------------------------------------------------------------
 */
 
-#include <esp_log.h>
-#include <esp_err.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include "esp32_i2c_rw.h"
+#include "i2c_utils.h"
 
-#define I2C_NUM I2C_NUM_0
-
-/*
- * @brief Select the register in the device where data will be read from.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the first register to read from.
- */
-void select_register (uint8_t device_address, uint8_t register_address)
+esp_err_t i2c_read_byte(i2c_port_t i2c_num, uint8_t dev_addr,
+									uint8_t reg_addr, uint8_t *data)
 {
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create ();
-	ESP_ERROR_CHECK (i2c_master_start (cmd));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, (device_address << 1)
-	| I2C_MASTER_WRITE, 1));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, register_address, 1));
-	ESP_ERROR_CHECK (i2c_master_stop (cmd));
-	ESP_ERROR_CHECK (i2c_master_cmd_begin (I2C_NUM, cmd, 1000
-	/ portTICK_PERIOD_MS));
-	i2c_cmd_link_delete (cmd);
+    return i2c_read_bytes(i2c_num, dev_addr, reg_addr, data, 1);
 }
 
-/*
- * @brief Read multiple bytes from 8-bit registers.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the first register to read from.
- * @param size: Number of registers to read.
- * @param data: Buffer to store the read data in.
- * @return Status of read operation.
- */
-int8_t esp32_i2c_read_bytes (uint8_t device_address, uint8_t register_address,
-uint8_t size, uint8_t* data)
+esp_err_t i2c_read_bytes(i2c_port_t i2c_num, uint8_t dev_addr, 
+                                uint8_t reg_addr, uint8_t *data, size_t size)
 {
-	select_register (device_address, register_address);
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create ();
-	ESP_ERROR_CHECK (i2c_master_start (cmd));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, (device_address << 1)
-	| I2C_MASTER_READ, 1));
+	if(!size)
+        return ESP_OK;
 
-	if (size > 1) {ESP_ERROR_CHECK (i2c_master_read (cmd, data, size - 1, 0));}
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_WRITE, 1);
+    i2c_master_write_byte(cmd, reg_addr, true);
 
-	ESP_ERROR_CHECK (i2c_master_read_byte (cmd, data + size - 1, 1));
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_READ, true);
 
-	ESP_ERROR_CHECK (i2c_master_stop (cmd));
-	ESP_ERROR_CHECK (i2c_master_cmd_begin (I2C_NUM, cmd, 1000
-	/ portTICK_PERIOD_MS));
-	i2c_cmd_link_delete (cmd);
+    if(size > 1)
+        i2c_master_read(cmd, data, size-1, 0); //0 is ACK
 
-	return (size);
+    i2c_master_read_byte(cmd, data+size-1, 1); //1 is NACK
+
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000/portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
 }
 
-/*
- * @brief Read single byte from an 8-bit register.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the register to read from.
- * @param data: Container to store the byte read from register.
- * @return Status of read operation.
- */
-int8_t esp32_i2c_read_byte (uint8_t device_address, uint8_t register_address,
-uint8_t* data)
+esp_err_t i2c_write_byte(i2c_port_t i2c_num, uint8_t dev_addr,
+									uint8_t reg_addr, uint8_t data)
 {
-	return (esp32_i2c_read_bytes (device_address, register_address, 1, data));
+	return i2c_write_bytes(i2c_num, dev_addr, reg_addr, &data, 1);
 }
 
-/*
- * @brief Read multiple bits from an 8-bit register.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the register to read from.
- * @param bit_start: First bit position to read (0-7).
- * @param size: Number of bits to read (Not more than 8).
- * @param data: Container to store the right-aligned value.
- * @return Status of read operation.
- */
-int8_t esp32_i2c_read_bits (uint8_t device_address, uint8_t register_address,
-uint8_t bit_start, uint8_t size, uint8_t* data)
+esp_err_t i2c_write_bytes(i2c_port_t i2c_num, uint8_t dev_addr, 
+                                uint8_t reg_addr, uint8_t *data, size_t size)
 {
-	// Example:
-	// 01101001 - Read byte.
-	// 76543210 - Bit numbers.
-	//    xxx   - Arguments: bit_start = 4, size = 3.
-	//    010   - Mask.
-	//    ->010 - Right shift.
-	uint8_t bit;
-	uint8_t count;
+	// start transmission to device
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (i2c_num << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, reg_addr, true); // send register address
 
-	if ((count = esp32_i2c_read_byte (device_address, register_address, &bit))
-	!= 0)
-	{
-		uint8_t mask = ((1 << size) - 1) << (bit_start - size + 1);
+	if(size > 1)
+        i2c_master_write(cmd, data, size-1, 0); //0 is ACK
 
-		bit &= mask;
-		bit >>= (bit_start - size + 1);
-		*data = bit;
-	}
+    i2c_master_write_byte(cmd, *(data+size-1), 1);
 
-	return (count);
+    // end transmission
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000/portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
 }
 
-/*
- * @brief Read single bit from an 8-bit register.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the register to read from.
- * @param bit_number: Bit position to read (0-7).
- * @param data: Container to store the bit.
- * @return Status of read operation.
- */
-int8_t esp32_i2c_read_bit (uint8_t device_address, uint8_t register_address,
-uint8_t bit_number, uint8_t* data)
+esp_err_t i2c_read_bit(i2c_port_t i2c_num, uint8_t dev_addr, uint8_t reg_addr,
+                                				uint8_t *data, uint8_t offset)
 {
-	uint8_t bit;
-	uint8_t count = esp32_i2c_read_byte (device_address, register_address,
-	&bit);
-
-	*data = bit & (1 << bit_number);
-
-	return (count);
+	return i2c_read_bits(i2c_num, dev_addr, reg_addr, data, offset, 1);
 }
 
-/*
- * @brief Write multiple bytes to 8-bit registers.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the first register to write to.
- * @param size: Number of bytes to write.
- * @param data: Array of bytes to write.
- * @return Status of write operation.
- */
-bool esp32_i2c_write_bytes (uint8_t device_address, uint8_t register_address,
-uint8_t size, uint8_t* data)
+esp_err_t i2c_read_bits(i2c_port_t i2c_num, uint8_t dev_addr, uint8_t reg_addr,
+                                uint8_t *data, uint8_t offset, uint8_t length)
 {
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create ();
-	ESP_ERROR_CHECK (i2c_master_start (cmd));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, (device_address << 1)
-	| I2C_MASTER_WRITE, 1));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, register_address, 1));
-	ESP_ERROR_CHECK (i2c_master_write (cmd, data, size - 1, 0));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, data [size - 1], 1));
-	ESP_ERROR_CHECK (i2c_master_stop (cmd));
-	ESP_ERROR_CHECK (i2c_master_cmd_begin (I2C_NUM, cmd, 1000
-	/ portTICK_PERIOD_MS));
-	i2c_cmd_link_delete (cmd);
+	*data = 0;
 
-	return (true);
+	esp_err_t ret;
+	ret = i2c_read_byte(i2c_num, dev_addr, reg_addr, data);
+	if(ret != 1)
+		return ret;
+
+	*data &= ((1 << length) - 1) << (offset - length + 1); // Mask
+	*data >>= (offset - length + 1); // Set to first bits
+
+	return 1;
 }
 
-/*
- * @brief Write single byte to an 8-bit register.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the register to write to.
- * @param data: Array of bytes to write.
- * @return Status of write operation.
- */
-bool esp32_i2c_write_byte (uint8_t device_address, uint8_t register_address,
-uint8_t data)
-{
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create ();
-	ESP_ERROR_CHECK (i2c_master_start (cmd));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, (device_address << 1)
-	| I2C_MASTER_WRITE, 1));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, register_address, 1));
-	ESP_ERROR_CHECK (i2c_master_write_byte (cmd, data, 1));
-	ESP_ERROR_CHECK (i2c_master_stop (cmd));
-	ESP_ERROR_CHECK (i2c_master_cmd_begin (I2C_NUM, cmd, 1000
-	/ portTICK_PERIOD_MS));
-	i2c_cmd_link_delete (cmd);
 
-	return (true);
+esp_err_t i2c_write_bits(i2c_port_t i2c_num, uint8_t dev_addr,
+								uint8_t reg_addr, uint8_t mask, bool enable)
+{
+	esp_err_t ret;
+	uint8_t reg;
+	ret = i2c_read_byte(i2c_num, dev_addr, reg_addr, &reg);
+	if(ret != 1)
+		return ret;
+
+	if(enable)
+		reg |= mask;
+	else
+		reg &= ~(mask);
+
+	return i2c_write_byte(i2c_num, dev_addr, reg_addr, reg);
 }
 
-/*
- * @brief Write multiple bits to an 8-bit register.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the register to write to.
- * @param bit_start: First bit position to write (0-7).
- * @param size: Number of bits to write (Not more than 8).
- * @param data: Right-aligned value to write.
- * @return Status of write operation.
- */
-bool esp32_i2c_write_bits (uint8_t device_address, uint8_t register_address,
-uint8_t bit_start, uint8_t size, uint8_t data)
-{
-	uint8_t bit = 0;
-
-	if (esp32_i2c_read_byte (device_address, register_address, &bit) != 0)
-	{
-		uint8_t mask = ((1 << size) - 1) << (bit_start - size + 1);
-		data <<= (bit_start - size + 1); // Shift data into correct position.
-		data &= mask; // Zero all non-important bits in data.
-		bit &= ~(mask); // Zero all important bits in existing byte.
-		bit |= data; // Combine data with existing byte.
-
-		return (esp32_i2c_write_byte (device_address, register_address, bit));
-	}
-	else {return (false);}
-}
-
-/*
- * @brief Write single bit to an 8-bit register.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the register to write to.
- * @param bit_number: Bit position to write (0-7).
- * @param data: Bit value to write.
- * @return Status of write operation.
- */
-bool esp32_i2c_write_bit (uint8_t device_address, uint8_t register_address,
-uint8_t bit_number, uint8_t data)
-{
-	uint8_t bit;
-
-	esp32_i2c_read_byte (device_address, register_address, &bit);
-
-	if (data != 0) {bit = (bit | (1 << bit_number));}
-	else {bit = (bit & ~(1 << bit_number));}
-
-	return (esp32_i2c_write_byte (device_address, register_address, bit));
-}
-
-/*
- * @brief Write word to an 8-bit register.
- *
- * @param device_address: I2C slave device address.
- * @param register_address: Address of the register to write to.
- * @param data: Word to write.
- * @return Status of write operation.
- */
-int8_t esp32_i2c_write_word (uint8_t device_address, uint8_t register_address,
-uint8_t data)
-{
-	uint8_t data_1 [] = {(uint8_t) (data >> 8), (uint8_t) (data & 0xFF)};
-	// uint8_t data_2 [] = {(uint8_t) (data & 0xFF), (uint8_t) (data >> 8)};
-
-	esp32_i2c_write_bytes (device_address, register_address, 2, data_1);
-
-	return (1);
-}
+/**
+ * -----------------------------------------------------------------------------
+ * I2Cdev device library code is placed under the MIT license.
+ * 
+ * Copyright (c) 2012 Jeff Rowberg
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * -----------------------------------------------------------------------------
+*/
--- components/i2c_utils/include/i2c_utils.h
+++ components/i2c_utils/include/i2c_utils.h
@@ -1,72 +1,174 @@
-/*
- * @files esp32_i2c_rw.c and esp32_i2c_rw.h
- * @author Gabriel Boni Vicari (133192@upf.br)
+/**
+ * @file i2c_utils.h
+ * 
+ * @author
+ * Gabriel Boni Vicari (133192@upf.br)
+ * Angelo Elias Dalzotto (150633@upf.br)
  * GEPID - Grupo de Pesquisa em Cultura Digital (http://gepid.upf.br/)
  * Universidade de Passo Fundo (http://www.upf.br/)
+ * 
+ * @copyright
+ * Copyright (c) 2012 Jeff Rowberg.
+ * 
  * @brief I2C Read/Write functions for ESP32 ESP-IDF.
  *
- * Based on I2Cdevlib's ESP32_ESP-IDF by Dariusz Kempa.
+ * @details Based on I2Cdevlib's ESP32_ESP-IDF by Dariusz Kempa.
  * I2Cdevlib's ESP32_ESP-IDF is based on I2Cdevlib's EFM32 by Nicolas Baldeck.
  * I2Cdevlib's EFM32 is based on I2Cdevlib's Arduino by Jeff Rowberg.
- */
-
-/*
---------------------------------------------------------------------------------
-I2Cdev device library code is placed under the MIT license.
-
-Copyright (c) 2012 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
---------------------------------------------------------------------------------
 */
 
-#ifndef ESP32_I2C_RW_H
-#define ESP32_I2C_RW_H
+#ifndef I2C_UTILS_H
+#define I2C_UTILS_H
 
 #include <driver/i2c.h>
 
-void select_register (uint8_t device_address, uint8_t register_address);
+/**
+ * @brief Reads a device register of 1 byte via i2c.
+ * 
+ * @param i2c_num is the i2c port the device is connected to.
+ * @param dev_addr is the device i2c address.
+ * @param reg_addr is the device register address to read.
+ * @param data is the address of the variable the register will be saved.
+ * 
+ * @returns
+ *     ESP_FAIL(-1) Parameter error
+ *     Others(>=0) The number of data bytes that pushed to the I2C slave buffer
+*/
+esp_err_t i2c_read_byte(i2c_port_t i2c_num, uint8_t dev_addr, 
+                                    uint8_t reg_addr, uint8_t *data);
 
-int8_t esp32_i2c_read_bytes (uint8_t device_address, uint8_t register_address,
-uint8_t size, uint8_t* data);
+/**
+ * @brief Read multiple bytes via i2c.
+ * 
+ * @param i2c_num is the i2c port the device is connected to.
+ * @param dev_addr is the device i2c address.
+ * @param reg_addr is the device register address to read.
+ * @param *data is the address of the variable the register will be saved.
+ * @param size is the number of bytes to be read.
+ * 
+ * @returns
+ *     ESP_FAIL(-1) Parameter error
+ *     Others(>=0) The number of data bytes that pushed to the I2C slave buffer
+*/
+esp_err_t i2c_read_bytes(i2c_port_t i2c_num, uint8_t dev_addr, 
+                                uint8_t reg_addr, uint8_t *data, size_t size);
 
-int8_t esp32_i2c_read_byte (uint8_t device_address, uint8_t register_address,
-uint8_t* data);
+/**
+ * @brief Write one byte via i2c.
+ * 
+ * @param i2c_num is the i2c port the device is connected to.
+ * @param dev_addr is the device i2c address.
+ * @param reg_addr is the device register address to write.
+ * @param data is the value to be written.
+ * 
+ * @returns
+ *     ESP_FAIL(-1) Parameter error
+ *     Others(>=0) The number of data bytes that pushed to the I2C slave buffer
+*/
+esp_err_t i2c_write_byte(i2c_port_t i2c_num, uint8_t dev_addr,
+									uint8_t reg_addr, uint8_t data);
 
-int8_t esp32_i2c_read_bits (uint8_t device_address, uint8_t register_address,
-uint8_t bit_start, uint8_t size, uint8_t* data);
+/**
+ * @brief Write multiple bytes via i2c.
+ * 
+ * @param i2c_num is the i2c port the device is connected to.
+ * @param dev_addr is the device i2c address.
+ * @param reg_addr is the device register address to write.
+ * @param *data is the address of the variable to be written.
+ * @param size is the number of bytes to write
+ * 
+ * @returns
+ *     ESP_FAIL(-1) Parameter error
+ *     Others(>=0) The number of data bytes that pushed to the I2C slave buffer
+*/
+esp_err_t i2c_write_bytes(i2c_port_t i2c_num, uint8_t dev_addr, 
+                                uint8_t reg_addr, uint8_t *data, size_t size);
 
-int8_t esp32_i2c_read_bit (uint8_t device_address, uint8_t register_address,
-uint8_t bit_number, uint8_t* data);
+/**
+ * @brief Read a bit from a register.
+ * 
+ * @details The byte read is masked with the offset desired and then
+ * shifted to the least significant bit.
+ * 
+ * @param i2c_num is the i2c port the device is connected to.
+ * @param dev_addr is the device i2c address.
+ * @param reg_addr is the device register address to read.
+ * @param *data is the address of the variable to be read.
+ * @param offset is the bit offset to read.
+ * 
+ * @returns
+ *     ESP_FAIL(-1) Parameter error
+ *     Others(>=0) The number of data bytes that pushed to the I2C slave buffer
+*/
+esp_err_t i2c_read_bit(i2c_port_t i2c_num, uint8_t dev_addr, uint8_t reg_addr,
+                                                uint8_t *data, uint8_t offset);
 
-bool esp32_i2c_write_bytes (uint8_t device_address, uint8_t register_address,
-uint8_t size, uint8_t* data);
+/**
+ * @brief Read a number of bits from a register.
+ * 
+ * @details The byte read is masked with the size and offset desired and then
+ * shifted to the least significant bits.
+ * 
+ * @param i2c_num is the i2c port the device is connected to.
+ * @param dev_addr is the device i2c address.
+ * @param reg_addr is the device register address to read.
+ * @param *data is the address of the variable to be read.
+ * @param offset is the most significant bit to read.
+ * @param length is the number of bits to read.
+ * 
+ * @returns
+ *     ESP_FAIL(-1) Parameter error
+ *     Others(>=0) The number of data bytes that pushed to the I2C slave buffer
+*/
+esp_err_t i2c_read_bits(i2c_port_t i2c_num, uint8_t dev_addr, uint8_t reg_addr,
+                                uint8_t *data, uint8_t offset, uint8_t length);
 
-bool esp32_i2c_write_byte (uint8_t device_address, uint8_t register_address,
-uint8_t data);
+/**
+ * @brief Masks a byte register via i2c.
+ * 
+ * @details The function reads the register and applies the mask to enable or
+ * disable the bits. The "mask" param must be 0b00001111 to set the last 4 bits
+ * in case "enable" param is true, or to disable the last 4 bits in case
+ * "enable" param is false.
+ * 
+ * @param i2c_num is the i2c port the device is connected to.
+ * @param dev_addr is the device i2c address.
+ * @param reg_addr is the device register address to modify.
+ * @param mask is the byte mask to enable or disable.
+ * @param enable is if the mask '1's will be enabled or disabled in the
+ * register.
+ * 
+ * @returns
+ *     ESP_FAIL(-1) Parameter error
+ *     Others(>=0) The number of data bytes that pushed to the I2C slave buffer
+*/
+esp_err_t i2c_write_bits(i2c_port_t i2c_num, uint8_t dev_addr,
+								uint8_t reg_addr, uint8_t mask, bool enable);
 
-bool esp32_i2c_write_bits (uint8_t device_address, uint8_t register_address,
-uint8_t bit_start, uint8_t size, uint8_t data);
-
-bool esp32_i2c_write_bit (uint8_t device_address, uint8_t register_address,
-uint8_t bit_number, uint8_t data);
-
-int8_t esp32_i2c_write_word (uint8_t device_address, uint8_t register_address,
-uint8_t data);
 #endif
+
+/**
+ * -----------------------------------------------------------------------------
+ * I2Cdev device library code is placed under the MIT license.
+ * 
+ * Copyright (c) 2012 Jeff Rowberg
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * -----------------------------------------------------------------------------
+*/
